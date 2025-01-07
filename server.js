9b3505a41ce49f1d2d298f2d951721873847eaf2
Ali <aoaliyu@student.oauife.edu.ng>
2025-01-07 21:30:03 +0100

    Done for today

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const articleRouter = require("./routes/articles");
 const app = express();
 
-app.set('view engine', 'ejs')
+// mongoose.connect('mongodb://localhost/blog', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
+// 
+// mongoose.connect('mongodb://localhost/blog')
 
+app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res)=>{
     const articles = [{
-        title: 'Test Article',
-        timeCreated: Date.now,
-        description: 'Test Description'
-    }]
-    res.render("index", { articles : articles })
+        title: 'Test Article1',
+        timeCreated: new Date(),
+        description: 'Test Description 1'
+    },
+    {
+        title: 'Test Article2',
+        timeCreated: new Date(),
+        description: 'Test Description 2'
+    }
+]
+    res.render("articles/index", { articles : articles })
 })
 
 app.listen(5000, ()=>{
